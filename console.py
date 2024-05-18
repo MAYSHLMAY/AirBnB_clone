afb4b14e6676c6ba0e7fb97fa46b2d8428cb917a
Mikiyas Abera <educationforlife424@gmail.com>
2024-05-18 21:27:33 +0300

    Blank lines removed

--- console.py
+++ console.py
@@ -74,7 +74,9 @@ class HBNBCommand(cmd.Cmd):
         return False
 
     def do_quit(self, arg):
-        """Quit command to exit the program."""
+        """Quit command to exit the program.
+        
+        """
         return True
 
     def do_EOF(self, arg):
