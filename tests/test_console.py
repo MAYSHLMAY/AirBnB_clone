b28aa3e68c68cc9434acef2b4f509cb5a5927f5a
Mikiyas Abera <educationforlife424@gmail.com>
2024-05-18 20:12:23 +0300

    Mock check

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,7 +7,7 @@ Checks console for capturing stdout into a StringIO object
 import os
 import sys
 import unittest
-from unittest.mock import create_autospec, patch
+# from unittest.mock import create_autospec, patch
 from io import StringIO
 from console import HBNBCommand
 # from models import storage
@@ -24,42 +24,7 @@ class TestConsole(unittest.TestCase):
     """
     Unittest for the console model
     """
-
-    def setUp(self):
-        """Redirecting stdin and stdout"""
-        self.mock_stdin = create_autospec(sys.stdin)
-        self.mock_stdout = create_autospec(sys.stdout)
-        self.err = ["** class name missing **",
-                    "** class doesn't exist **",
-                    "** instance id missing **",
-                    "** no instance found **",
-                    ]
-
-        self.cls = ["BaseModel",
-                    "User",
-                    "State",
-                    "City",
-                    "Place",
-                    "Amenity",
-                    "Review"]
-
-    def create(self, server=None):
-        """
-        Redirects stdin and stdout to the mock module
-        """
-        return HBNBCommand(stdin=self.mock_stdin, stdout=self.mock_stdout)
-
-    def last_write(self, nr=None):
-        """Returns last n output lines"""
-        if nr is None:
-            return self.mock_stdout.write.call_args[0][0]
-        return "".join(map(lambda c: c[0][0],
-                           self.mock_stdout.write.call_args_list[-nr:]))
-
-    def test_quit(self):
-        """Quit command"""
-        cli = self.create()
-        self.assertTrue(cli.onecmd("quit"))
+    pass
 
 
 if __name__ == '__main__':
