ce49938319f122c0b7dfcddd6d6492dee4f98768
Mikiyas Abera <educationforlife424@gmail.com>
2024-05-19 12:45:14 +0300

    Error prevails

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,7 +39,7 @@ class FileStorage:
     def new(self, obj):
         '''Set new __objects to existing dictionary of instances'''
         if obj:
-            key = 'f{obj.__class__.__name__}.{obj.id}'
+            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
